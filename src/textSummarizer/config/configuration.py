03b2c75e36e4200270252a726993844cff9c1a8a
Ujjwal Gupta <150505461+Ujjwagupta1302@users.noreply.github.com>
2025-05-03 15:19:06 +0530

    Model Training done

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,14 +1,14 @@
 from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml, create_directories
 
-from src.textSummarizer.entity import DataIngestionConfig
+from src.textSummarizer.entity import (DataIngestionConfig,DataTransformationConfig,ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(self,
                  config_path=CONFIG_FILE_PATH,
                  params_filepath=PARAMS_FILE_PATH):
         self.config=read_yaml(config_path)
-        self.paramss=read_yaml(params_filepath)
+        self.params=read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -25,3 +25,37 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+        config=self.config.data_transformation
+        create_directories([config.root_dir])
+
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+
+        return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config=self.config.model_trainer
+        params=self.params.TrainingArguments
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt=config.model_ckpt,
+            num_train_epochs=params.num_train_epochs,
+            warmup_steps=params.warmup_steps,
+            per_device_train_batch_size=params.per_device_train_batch_size,
+            weight_decay=params.weight_decay,
+            logging_steps=params.logging_steps,
+            evaluation_strategy=params.evaluation_strategy,
+            eval_steps=params.eval_steps,  # <-- fix here
+            save_steps=params.save_steps,
+            gradient_accumulation_steps=params.gradient_accumulation_steps
+        )
+        return model_trainer_config
